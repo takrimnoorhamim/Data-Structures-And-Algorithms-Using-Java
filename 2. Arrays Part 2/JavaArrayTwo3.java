eff78b21e321efa554d948f04d5627b9686a69df
Takrim Noor Hamim <takrimnoorhamimpass@gmail.com>
2025-08-28 06:06:47 +0600

    Update JavaArrayTwo3.java

--- 2. Arrays Part 2/JavaArrayTwo3.java
+++ 2. Arrays Part 2/JavaArrayTwo3.java
@@ -5,6 +5,32 @@ public class JavaArrayTwo3 {
     public static void main(String[] args) {
         
         int arr[] = {1,-2,6,-1,3};
+        KadanSubarraySum(arr);
+
+    }
+
+    public static void KadanSubarraySum (int[] arr){
+
+        int currentSum = 0;
+        int maxSum = Integer.MIN_VALUE;
+
+
+        for(int i = 0; i < arr.length; i++){
+
+            currentSum += arr[i];
+            
+            if(currentSum<0){
+                currentSum = 0;
+            }
+
+            if(currentSum > maxSum){
+                maxSum = currentSum;
+            }
+
+        }
+
+        System.out.println(maxSum);
+
 
     }
 
